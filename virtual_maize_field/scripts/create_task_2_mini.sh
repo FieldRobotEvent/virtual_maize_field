15110b9c5ef351e273a0955a9156c436bb938caa
Thijs Ruigrok <thijs.ruigrok@wur.nl>
2021-05-12 04:28:16 -0700

    create_task_*.sh files now clear the gazbeo cache, preventing gazebo from using old chached heightmap data.

--- virtual_maize_field/scripts/create_task_2_mini.sh
+++ virtual_maize_field/scripts/create_task_2_mini.sh
@@ -1,3 +1,6 @@
+# remove old gazebo cache file, so gazebo is forced to use the new height map.
+rm -r ~/.gazebo/paging/virtual_maize_field_heightmap
+
 rosrun virtual_maize_field generate_world.py \
 --row_length 3.5 \
 --rows_left 0 \
