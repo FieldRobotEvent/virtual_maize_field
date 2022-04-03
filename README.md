1552fc942f775ef3ef3b281da43adf96b5a93034
Rick van Essen <rick.vanessen@wur.nl>
2022-04-03 18:58:08 +0200

    Change to config files

--- README.md
+++ README.md
@@ -43,21 +43,45 @@ You can call the script using
 ```bash
 rosrun virtual_maize_field generate_world.py
 ```
-The resulting file will be placed in `worlds/generated.world`.
+The resulting file will be placed in `worlds/generated.world`. You can use this script by one of the defined config files or specifying the parameters below:
 ```
-usage: generate_world.py [-h] [--row_length ROW_LENGTH] [--rows_curve_budget ROWS_CURVE_BUDGET] [--row_width ROW_WIDTH] [--rows_count ROWS_COUNT] [--row_segments ROW_SEGMENTS]
-                         [--row_segment_straight_length_min ROW_SEGMENT_STRAIGHT_LENGTH_MIN] [--row_segment_straight_length_max ROW_SEGMENT_STRAIGHT_LENGTH_MAX]
-                         [--row_segment_curved_radius_min ROW_SEGMENT_CURVED_RADIUS_MIN] [--row_segment_curved_radius_max ROW_SEGMENT_CURVED_RADIUS_MAX]
-                         [--row_segment_curved_arc_measure_min ROW_SEGMENT_CURVED_ARC_MEASURE_MIN] [--row_segment_curved_arc_measure_max ROW_SEGMENT_CURVED_ARC_MEASURE_MAX]
-                         [--row_segment_island_radius_min ROW_SEGMENT_ISLAND_RADIUS_MIN] [--row_segment_island_radius_max ROW_SEGMENT_ISLAND_RADIUS_MAX]
-                         [--ground_resolution GROUND_RESOLUTION] [--ground_elevation_max GROUND_ELEVATION_MAX] [--ground_headland GROUND_HEADLAND]
-                         [--ground_ditch_depth GROUND_DITCH_DEPTH] [--plant_spacing_min PLANT_SPACING_MIN] [--plant_spacing_max PLANT_SPACING_MAX]
-                         [--plant_height_min PLANT_HEIGHT_MIN] [--plant_height_max PLANT_HEIGHT_MAX] [--plant_radius PLANT_RADIUS] [--plant_radius_noise PLANT_RADIUS_NOISE]
-                         [--plant_placement_error_max PLANT_PLACEMENT_ERROR_MAX] [--plant_mass PLANT_MASS] [--hole_prob HOLE_PROB] [--hole_size_max HOLE_SIZE_MAX]
-                         [--crop_types CROP_TYPES] [--litters LITTERS] [--litter_types LITTER_TYPES] [--weeds WEEDS] [--weed_types WEED_TYPES] [--ghost_objects GHOST_OBJECTS]
-                         [--location_markers LOCATION_MARKERS] [--load_from_file LOAD_FROM_FILE] [--seed SEED]
+usage: generate_world.py [-h] [--row_length ROW_LENGTH]
+                         [--rows_curve_budget ROWS_CURVE_BUDGET]
+                         [--row_width ROW_WIDTH] [--rows_count ROWS_COUNT]
+                         [--row_segments ROW_SEGMENTS]
+                         [--row_segment_straight_length_min ROW_SEGMENT_STRAIGHT_LENGTH_MIN]
+                         [--row_segment_straight_length_max ROW_SEGMENT_STRAIGHT_LENGTH_MAX]
+                         [--row_segment_curved_radius_min ROW_SEGMENT_CURVED_RADIUS_MIN]
+                         [--row_segment_curved_radius_max ROW_SEGMENT_CURVED_RADIUS_MAX]
+                         [--row_segment_curved_arc_measure_min ROW_SEGMENT_CURVED_ARC_MEASURE_MIN]
+                         [--row_segment_curved_arc_measure_max ROW_SEGMENT_CURVED_ARC_MEASURE_MAX]
+                         [--row_segment_island_radius_min ROW_SEGMENT_ISLAND_RADIUS_MIN]
+                         [--row_segment_island_radius_max ROW_SEGMENT_ISLAND_RADIUS_MAX]
+                         [--ground_resolution GROUND_RESOLUTION]
+                         [--ground_elevation_max GROUND_ELEVATION_MAX]
+                         [--ground_headland GROUND_HEADLAND]
+                         [--ground_ditch_depth GROUND_DITCH_DEPTH]
+                         [--plant_spacing_min PLANT_SPACING_MIN]
+                         [--plant_spacing_max PLANT_SPACING_MAX]
+                         [--plant_height_min PLANT_HEIGHT_MIN]
+                         [--plant_height_max PLANT_HEIGHT_MAX]
+                         [--plant_radius PLANT_RADIUS]
+                         [--plant_radius_noise PLANT_RADIUS_NOISE]
+                         [--plant_placement_error_max PLANT_PLACEMENT_ERROR_MAX]
+                         [--plant_mass PLANT_MASS] [--hole_prob HOLE_PROB]
+                         [--hole_size_max HOLE_SIZE_MAX]
+                         [--crop_types CROP_TYPES] [--litters LITTERS]
+                         [--litter_types LITTER_TYPES] [--weeds WEEDS]
+                         [--weed_types WEED_TYPES]
+                         [--ghost_objects GHOST_OBJECTS]
+                         [--location_markers LOCATION_MARKERS]
+                         [--load_from_file LOAD_FROM_FILE] [--seed SEED]
+                         [config_file]
 
-Generate a virtual maize field world for gazebo
+Generate a virtual maize field world for Gazebo.
+
+positional arguments:
+  config_file           Config file name in the config folder
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -133,7 +157,7 @@ optional arguments:
 ```
 
 ## Sample Worlds
-In the [bin folder](bin/), bash files to generate sample worlds are located. The parameters are chosen to match [the task description](https://www.fieldrobot.com/event/index.php/contest/)
+In the [config folder](config/), config files to generate sample worlds are located. The parameters are chosen to match [the task description](https://www.fieldrobot.com/event/index.php/contest/)
 
 Worlds for the Field Robot Event 2022:
 | Name | Description |
@@ -148,6 +172,11 @@ Other sample Worlds:
 | *fre21_task_2* | Task 2, straight rows with holes |
 | *fre21_task_3* | Task 3, similar crop rows as in task_2 but with cans, bottles and weeds spread throughout the field. The cans, bottles and weeds have no collision box and are static. |
 | *fre21_task_4* | Task 4, similar crop rows as in task_2 but with cans, bottles and weeds spread throughout the field. The cans, bottles and weeds have a collision box and can be picked up. |
+
+You can use these config files when generating worlds, e.g.:
+```commandline
+rosrun virtual_maize_field generate_world.py fre22_task_navigation_mini
+```
 
 ## Launching worlds
 The launch file to launch the worlds is called `simulation.launch`. You can launch the launch file by running `roslaunch virtual_maize_field jackal_simulation.launch`. By default the launch file will launch `generated_world.world`. You can launch any world by using the `world_name` arg. e.g. `roslaunch virtual_maize_field jackal_simulation.launch world_name:=simple_row_level_1.world`.
--- src/virtual_maize_field/generate_world.py
+++ src/virtual_maize_field/generate_world.py
@@ -1,27 +1,151 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 
-import argparse
-import csv
-import inspect
-import os
-import shutil
+from csv import writer as csv_writer
+from distutils.command.config import config
+from pathlib import Path
+from shutil import rmtree
 
 import cv2
 import numpy as np
 import rospkg
+import yaml
 
 from virtual_maize_field.world_generator.field_2d_generator import Field2DGenerator
 from virtual_maize_field.world_generator.world_description import WorldDescription
 
+LAUNCH_FILE_TEMPLATE = """<?xml version="1.0"?>
+<launch>
+    <!-- Spawn Robot -->
+    <arg name="robot_name" default="robot_model" />
+    <node name="urdf_spawner" pkg="gazebo_ros" type="spawn_model"
+    args="-urdf -model $(arg robot_name) -param robot_description -x {x} -y {y} -z {x} -R 0 -P 0 -Y {yaw}" /> 
+</launch>
+"""
+
+
+class WorldGenerator:
+    def __init__(self, **kwargs) -> None:
+        self.wd = WorldDescription(**kwargs)
+        self.fgen = Field2DGenerator(self.wd)
+
+        self.pkg_path = Path(rospkg.RosPack().get_path("virtual_maize_field"))
+
+    def generate(self) -> None:
+        # Generate the template and write it to a file
+        generated_sdf, heightmap = self.fgen.generate()
+
+        sdf_file = self.pkg_path / "worlds/generated.world"
+        with sdf_file.open("w") as f:
+            f.write(generated_sdf)
+
+        # Save heightmap
+        heightmap_file = pkg_path / "Media/models/virtual_maize_field_heightmap.png"
+        cv2.imwrite(str(heightmap_file), self.fgen.heightmap)
+
+    def clear_gazebo_cache(self) -> None:
+        # Clear the gazbeo cache for old heightmap
+        gazebo_cache_pkg = Path.home() / ".gazebo/paging/virtual_maize_field_heightmap"
+        if gazebo_cache_pkg.is_dir():
+            rmtree(gazebo_cache_pkg)
+
+    def save_minimap(self) -> None:
+        minimap_file = pkg_path / "map/map.png"
+        self.fgen.minimap.savefig(str(minimap_file), dpi=100)
+
+    def save_marker_file(self) -> None:
+        marker_file = self.pkg_path / "map/markers.csv"
+        with marker_file.open("w") as f:
+            writer = csv_writer(f)
+            header = ["X", "Y", "kind"]
+            writer.writerow(header)
+            if self.fgen.marker_a_loc.shape[0] != 0:
+                writer.writerow(
+                    [
+                        self.fgen.marker_a_loc[0][0],
+                        self.fgen.marker_a_loc[0][1],
+                        "location_marker_a",
+                    ]
+                )
+                writer.writerow(
+                    [
+                        self.fgen.marker_b_loc[0][0],
+                        self.fgen.marker_b_loc[0][1],
+                        "location_marker_b",
+                    ]
+                )
+
+    def save_complete_map(self) -> None:
+        complete_map_file = pkg_path / "map/map.csv"
+        with complete_map_file.open("w") as f:
+            writer = csv_writer(f)
+            header = ["X", "Y", "kind"]
+            writer.writerow(header)
+
+            # marker
+            if self.fgen.marker_a_loc.shape[0] != 0:
+                writer.writerow(
+                    [
+                        self.fgen.marker_a_loc[0][0],
+                        self.fgen.marker_a_loc[0][1],
+                        "location_marker_a",
+                    ]
+                )
+                writer.writerow(
+                    [
+                        self.fgen.marker_b_loc[0][0],
+                        self.fgen.marker_b_loc[0][1],
+                        "location_marker_b",
+                    ]
+                )
+
+            for elm in self.fgen.weed_placements:
+                writer.writerow([elm[0], elm[1], "weed"])
+
+            for elm in self.fgen.litter_placements:
+                writer.writerow([elm[0], elm[1], "litter"])
+
+            for elm in self.fgen.crop_placements:
+                writer.writerow([elm[0], elm[1], "crop"])
+
+    def save_launch_file(self) -> None:
+        launch_file = pkg_path / "launch/robot_spawner.launch"
+        with launch_file.open("w") as f:
+            content = LAUNCH_FILE_TEMPLATE.format(
+                x=float(self.fgen.start_loc[0][0]) + np.random.rand() * 0.1 - 0.05,
+                y=float(self.fgen.start_loc[0][1]) + np.random.rand() * 0.1 - 0.05,
+                z=0.7,
+                yaw=1.5707963267948966 + np.random.rand() * 0.1 - 0.05,
+            )
+            f.write(content)
+
+    @classmethod
+    def from_config_file(cls, config_file: Path) -> WorldGenerator:
+        with config_file.open("r") as f:
+            config = yaml.safe_load(f)
+        return WorldGenerator(**config)
+
+
 if __name__ == "__main__":
-    # get the possible arguments of the generator and default values
-    argspec = inspect.getfullargspec(WorldDescription.__init__)
+    from argparse import ArgumentParser
+    from inspect import getfullargspec
+
+    pkg_path = Path(rospkg.RosPack().get_path("virtual_maize_field"))
+
+    # Dynamically create argument parser with world description parameters
+    argspec = getfullargspec(WorldDescription.__init__)
     possible_kwargs = argspec.args[1:]
     defaults = argspec.defaults
 
-    # construct an ArgumentParser that takes these arguments
-    parser = argparse.ArgumentParser(
-        description="Generate a virtual maize field world for gazebo"
+    parser = ArgumentParser(
+        description="Generate a virtual maize field world for Gazebo."
+    )
+    parser.add_argument(
+        "config_file",
+        nargs="?",
+        type=str,
+        help="Config file name in the config folder",
+        default=None,
     )
     for argname, default in zip(possible_kwargs, defaults):
         # we analyze the default value's type to guess the type for that argument
@@ -32,89 +156,28 @@ if __name__ == "__main__":
             required=False,
         )
 
-    # get a dict representation of the arguments and call our constructor with them as kwargs
-    args = vars(parser.parse_args())
-    args = {k: v for k, v in args.items() if v is not None}
-    wd = WorldDescription(**args)
-    fgen = Field2DGenerator(wd)
+    args = parser.parse_args()
 
-    # generate the template and write it to a file
-    pkg_path = rospkg.RosPack().get_path("virtual_maize_field")
-    generated_sdf, heightmap = fgen.generate()
-    sdf_path = os.path.join(pkg_path, "worlds/generated.world")
-    with open(sdf_path, "w") as f:
-        f.write(generated_sdf)
-    # save heightmap
-    heightmap_path = os.path.join(
-        pkg_path, "Media/models/virtual_maize_field_heightmap.png"
-    )
-    cv2.imwrite(heightmap_path, fgen.heightmap)
+    if args.config_file:
+        if ".yaml" in args.config_file:
+            config_file_path = pkg_path / "config" / args.config_file
+        else:
+            config_file_path = pkg_path / "config" / (args.config_file + ".yaml")
 
-    # clear the gazbeo cache for old heightmap
-    home_dir = os.path.expanduser("~")
-    gazebo_cache_pkg = os.path.join(
-        home_dir, ".gazebo/paging/virtual_maize_field_heightmap"
-    )
-    if os.path.isdir(gazebo_cache_pkg):
-        shutil.rmtree(gazebo_cache_pkg)
+        if not config_file_path.is_file():
+            print(f"ERROR: cannot find config: '{config_file_path}' ")
+            exit(1)
 
-    # save mini_map
-    minimap_path = os.path.join(pkg_path, "map/map.png")
-    fgen.minimap.savefig(minimap_path, dpi=100)
+        generator = WorldGenerator.from_config_file(config_file_path)
+    else:
+        # Get a dict representation of the arguments and call our constructor with them as kwargs
+        args = vars(parser.parse_args())
+        args = {k: v for k, v in args.items() if v is not None}
+        generator = WorldGenerator(**args)
 
-    # marker file
-    f_path = os.path.join(pkg_path, "map/markers.csv")
-    with open(f_path, "w") as f:
-        writer = csv.writer(f)
-        header = ["X", "Y", "kind"]
-        writer.writerow(header)
-        if fgen.marker_a_loc.shape[0] != 0:
-            writer.writerow(
-                [fgen.marker_a_loc[0][0], fgen.marker_a_loc[0][1], "location_marker_a"]
-            )
-            writer.writerow(
-                [fgen.marker_b_loc[0][0], fgen.marker_b_loc[0][1], "location_marker_b"]
-            )
-
-    # complete map
-    f_path = os.path.join(pkg_path, "map/map.csv")
-    with open(f_path, "w") as f:
-        writer = csv.writer(f)
-        header = ["X", "Y", "kind"]
-        writer.writerow(header)
-
-        # marker
-        if fgen.marker_a_loc.shape[0] != 0:
-            writer.writerow(
-                [fgen.marker_a_loc[0][0], fgen.marker_a_loc[0][1], "location_marker_a"]
-            )
-            writer.writerow(
-                [fgen.marker_b_loc[0][0], fgen.marker_b_loc[0][1], "location_marker_b"]
-            )
-
-        for elm in fgen.weed_placements:
-            writer.writerow([elm[0], elm[1], "weed"])
-
-        for elm in fgen.litter_placements:
-            writer.writerow([elm[0], elm[1], "litter"])
-
-        for elm in fgen.crop_placements:
-            writer.writerow([elm[0], elm[1], "crop"])
-
-    # save the start location in a launch file
-    launch_path = os.path.join(pkg_path, "launch/robot_spawner.launch")
-    with open(launch_path, "w") as launch_file:
-        string = """<?xml version="1.0"?>
-<launch>
-    <!-- Spawn Robot -->
-    <arg name="robot_name" default="robot_model" />
-    <node name="urdf_spawner" pkg="gazebo_ros" type="spawn_model"
-    args="-urdf -model $(arg robot_name) -param robot_description -x %f -y %f -z %f -R 0 -P 0 -Y %f" /> 
-</launch>""" % (
-            float(fgen.start_loc[0][0]) + np.random.rand() * 0.1 - 0.05,
-            float(fgen.start_loc[0][1]) + np.random.rand() * 0.1 - 0.05,
-            0.7,
-            1.5707963267948966 + np.random.rand() * 0.1 - 0.05,
-        )
-
-        launch_file.write(string)
+    generator.generate()
+    generator.clear_gazebo_cache()
+    generator.save_minimap()
+    generator.save_marker_file()
+    generator.save_complete_map()
+    generator.save_launch_file()
