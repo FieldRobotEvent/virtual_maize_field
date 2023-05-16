abfddb69a65c4c0b638468ac64289ac075cc6674
Rick-v-E <78429257+Rick-v-E@users.noreply.github.com>
2023-05-16 14:51:34 +0200

    Fix world generation ROS2 (#46)
    * Fix model path in ROS2
    
    * Clarify readme
    
    * Bump version
    
    * Fix issue with isort action

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
-
 from launch import LaunchDescription
+from launch_ros.actions import Node
 
 
 def generate_launch_description() -> LaunchDescription:
