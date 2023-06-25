842f556aa45b0a89cee0636a513aae727deddfa1
Rick-v-E <78429257+Rick-v-E@users.noreply.github.com>
2023-06-25 19:00:52 +0200

    Test build (#51)

--- test/test_config.py
+++ test/test_config.py
@@ -17,5 +17,9 @@ def test_predefinded_worlds(config_file: str) -> None:
     if "fre22_task_mapping" in config_file:
         pytest.skip()
 
+    # TODO: remove once https://github.com/FieldRobotEvent/virtual_maize_field/issues/45 is fixed
+    if not "_mini" in config_file:
+        pytest.skip()
+
     process = run(["ros2", "run", "virtual_maize_field", "generate_world", config_file])
     assert process.returncode == 0, f"Cannot create {config_file}!"
