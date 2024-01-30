441ba5a9b4bb20b47b343a56d18ce406b98bb4fc
Rick-v-E <78429257+Rick-v-E@users.noreply.github.com>
2024-01-30 18:07:15 +0100

    Add Ignition Gazebo branch for ROS Humble (#57)
    Port to Ignition Gazebo

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -3,57 +3,106 @@ from __future__ import annotations
 from os import environ, path
 
 from ament_index_python.packages import get_package_share_directory
-
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch import LaunchDescription, SomeSubstitutionsType
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, LogInfo
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import (
+    LaunchConfiguration,
+    PathJoinSubstitution,
+    PythonExpression,
+)
+
+from launch_ros.actions import Node
+
+
+def construct_gz_args(
+    world_file: SomeSubstitutionsType,
+    paused: SomeSubstitutionsType,
+    headless: SomeSubstitutionsType,
+) -> SomeSubstitutionsType:
+    paused_arg = PythonExpression(['"" if "', paused, '".lower() == "true" else "-r "'])
+    headless_arg = PythonExpression(
+        ['"-s " if "', headless, '".lower() == "true" else ""']
+    )
+
+    return PythonExpression(["'", headless_arg, paused_arg, world_file, "'"])
 
 
 def generate_launch_description() -> LaunchDescription:
     _ros_home_path = environ.get("ROS_HOME", path.join(path.expanduser("~"), ".ros"))
     cache_dir = path.join(_ros_home_path, "virtual_maize_field/")
 
-    use_sim_time = LaunchConfiguration("use_sim_time", default=True)
-    gui = LaunchConfiguration("gui", default=True)
-    paused = LaunchConfiguration("paused", default=False)
-    headless = LaunchConfiguration("headless", default=False)
-    world_path = LaunchConfiguration("world_path", default=cache_dir)
-    world_name = LaunchConfiguration("world_name", default="generated.world")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    paused = LaunchConfiguration("paused")
+    headless = LaunchConfiguration("headless")
+    world_path = LaunchConfiguration("world_path")
+    world_name = LaunchConfiguration("world_name")
 
-    gz_server_launch_file = path.join(
-        get_package_share_directory("gazebo_ros"), "launch", "gzserver.launch.py"
+    # Create launch arguments
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        name="use_sim_time", default_value="True"
     )
-    gz_client_launch_file = path.join(
-        get_package_share_directory("gazebo_ros"), "launch", "gzclient.launch.py"
+    declare_paused_cmd = DeclareLaunchArgument(
+        name="paused", default_value="False", description="Start simulation paused."
+    )
+    declare_headless_cmd = DeclareLaunchArgument(
+        name="headless",
+        default_value="False",
+        description="Start headless simulation (without rendering).",
+    )
+    declare_world_path_cmd = DeclareLaunchArgument(
+        name="world_path",
+        default_value=cache_dir,
+        description="Path to the directory containing the world SDF files.",
+    )
+    declare_world_name_cmd = DeclareLaunchArgument(
+        name="world_name",
+        default_value="generated.world",
+        description="Name of the world file.",
     )
 
-    gz_server_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([gz_server_launch_file]),
+    gz_args = construct_gz_args(
+        PathJoinSubstitution([world_path, world_name]),
+        paused,
+        headless,
+    )
+
+    log_gz_args = LogInfo(msg=["Start Ignition Gazebo with gz_args: '", gz_args, "'"])
+
+    # Create nodes
+    gz = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            path.join(
+                get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py"
+            ),
+        ),
         launch_arguments={
-            "world": [world_path, world_name],
-            "pause": paused,
-            "verbose": "true",
+            "gz_args": gz_args,
             "use_sim_time": use_sim_time,
         }.items(),
     )
 
-    gz_client_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([gz_client_launch_file]),
-        condition=IfCondition(PythonExpression([gui, " and not ", headless])),
-        launch_arguments={"use_sim_time": use_sim_time}.items(),
+    sim_time_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        name="clock_bridge",
+        condition=IfCondition(use_sim_time),
+        arguments=["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"],
     )
 
-    return LaunchDescription(
-        [
-            DeclareLaunchArgument(name="use_sim_time", default_value=use_sim_time),
-            DeclareLaunchArgument(name="gui", default_value=gui),
-            DeclareLaunchArgument(name="paused", default_value=paused),
-            DeclareLaunchArgument(name="headless", default_value=headless),
-            DeclareLaunchArgument(name="world_path", default_value=world_path),
-            DeclareLaunchArgument(name="world_name", default_value=world_name),
-            gz_server_launch,
-            gz_client_launch,
-        ]
-    )
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_paused_cmd)
+    ld.add_action(declare_headless_cmd)
+    ld.add_action(declare_world_path_cmd)
+    ld.add_action(declare_world_name_cmd)
+
+    # Add nodes
+    ld.add_action(log_gz_args)
+    ld.add_action(gz)
+    ld.add_action(sim_time_bridge)
+
+    return ld
